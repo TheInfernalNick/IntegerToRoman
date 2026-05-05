5626e433bd2ba8142d36803bb60aa7b8c1b66a0b
TheInfernalNick <theinfernalnick@gmail.com>
2026-05-05 11:17:08 +0200

    General polishing and updated for user interaction

--- roman-number/src/main/java/it/unipd/mtss/App.java
+++ roman-number/src/main/java/it/unipd/mtss/App.java
@@ -1,14 +1,20 @@
 package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
-    private RomanPrinter romanPrinter;
     public static void main( String[] args )
     {
-        new RomanPrinter();
+        int input = 1;
+        System.out.println("Benvenuto nel convertitore di numeri da arabi a romani");
+        while(input > 0){
+            System.out.println("Inserisci un numero e verrà convertito, inserisci un numero minore di 1 e interromperai l'esecuzione ");    
+            Scanner scan = new Scanner(System.in);
+            input = scan.nextInt();
+            if(input > 0)
+                new RomanPrinter(input);
+        }
+        
     }
 }
--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -1,10 +1,11 @@
 package it.unipd.mtss; 
 
-import java.util.Scanner;
-
 public class RomanPrinter {
 
     public RomanPrinter(){}
+    public RomanPrinter(int input){
+        print(input);
+    }
     public String print(int num){ 
         IntegerToRoman itr = new IntegerToRoman();
         String convertedString = itr.convert(num);
@@ -22,72 +23,73 @@ public class RomanPrinter {
         char b = '\\'; 
 
         for ( char c : romanNumber.toCharArray()){
-        switch(c){
-            case 'i' : 
-                    output1 += "  _____ ";
-                    output2 += " |_   _|";
-                    output3 += "   | |  ";
-                    output4 += "   | |  ";
-                    output5 += "  _| |_ ";  
-                    output6 += " |_____|";
-                 break;
-             case 'v' : 
-                    output1 += "  __      __ ";
-                    output2 += "  "+b+" "+b+"    / / ";
-                    output3 += "   "+b+" "+b+"  / /  ";
-                    output4 += "    "+b+" "+b+"/ /   ";
-                    output5 += "     \\  /    ";  
-                    output6 += "      \\/     ";
-                 break; 
-            
-            case 'x' : 
-                    output1 += "  __   __ ";
-                    output2 += "  "+b+" "+b+" / / ";
-                    output3 += "   "+b+" V /  ";
-                    output4 += "    > <   ";
-                    output5 += "   / . "+b+"  ";  
-                    output6 += "  /_/ "+b+"_"+b+ " ";
-                 break;   
-            
-            case 'l' : 
-                    output1 += "  _      ";
-                    output2 += " | |     ";
-                    output3 += " | |     ";
-                    output4 += " | |     ";
-                    output5 += " | |___  ";  
-                    output6 += " |_____| ";
-                 break;
-                                
-            case 'c' : 
-                    output1 += "   _____   ";
-                    output2 += "  / ____|  ";
-                    output3 += " | |       ";
-                    output4 += " | |       ";
-                    output5 += " | |____   ";  
-                    output6 += "  "+b+"_____|  ";
-                 break;
-            
-            case 'd' : 
-                    output1 += "  _____   ";
-                    output2 += " | __  "+b+"  ";
-                    output3 += " | |  | | ";
-                    output4 += " | |  | | ";
-                    output5 += " | |__| | ";  
-                    output6 += " |_____/  ";
-                 break;
-            
-            case 'm' : 
-                    output1 += "  __  __  ";
-                    output2 += " |  "+b+"/  | ";
-                    output3 += " | "+b+"  / | ";
-                    output4 += " | |"+b+"/| | ";
-                    output5 += " | |  | | ";  
-                    output6 += " |_|  |_| ";
-                 break;
-        };   
-    }
-    String output = output1+"\n"+output2+"\n"+output3+"\n"+output4+"\n"+output5+"\n"+output6;
-    System.out.println(output);
-    return output;
+            switch(c){
+                case 'i' : 
+                        output1 += "  _____ ";
+                        output2 += " |_   _|";
+                        output3 += "   | |  ";
+                        output4 += "   | |  ";
+                        output5 += "  _| |_ ";  
+                        output6 += " |_____|";
+                    break;
+                case 'v' : 
+                        output1 += "  __      __ ";
+                        output2 += "  "+b+" "+b+"    / / ";
+                        output3 += "   "+b+" "+b+"  / /  ";
+                        output4 += "    "+b+" "+b+"/ /   ";
+                        output5 += "     \\  /    ";  
+                        output6 += "      \\/     ";
+                    break; 
+                
+                case 'x' : 
+                        output1 += "  __   __ ";
+                        output2 += "  "+b+" "+b+" / / ";
+                        output3 += "   "+b+" V /  ";
+                        output4 += "    > <   ";
+                        output5 += "   / . "+b+"  ";  
+                        output6 += "  /_/ "+b+"_"+b+ " ";
+                    break;   
+                
+                case 'l' : 
+                        output1 += "  _      ";
+                        output2 += " | |     ";
+                        output3 += " | |     ";
+                        output4 += " | |     ";
+                        output5 += " | |___  ";  
+                        output6 += " |_____| ";
+                    break;
+                                    
+                case 'c' : 
+                        output1 += "   _____   ";
+                        output2 += "  / ____|  ";
+                        output3 += " | |       ";
+                        output4 += " | |       ";
+                        output5 += " | |____   ";  
+                        output6 += "  "+b+"_____|  ";
+                    break;
+                
+                case 'd' : 
+                        output1 += "  _____   ";
+                        output2 += " | __  "+b+"  ";
+                        output3 += " | |  | | ";
+                        output4 += " | |  | | ";
+                        output5 += " | |__| | ";  
+                        output6 += " |_____/  ";
+                    break;
+                
+                case 'm' : 
+                        output1 += "  __  __  ";
+                        output2 += " |  "+b+"/  | ";
+                        output3 += " | "+b+"  / | ";
+                        output4 += " | |"+b+"/| | ";
+                        output5 += " | |  | | ";  
+                        output6 += " |_|  |_| ";
+                    break;
+            };   
+        }
+        
+        String output = output1+"\n"+output2+"\n"+output3+"\n"+output4+"\n"+output5+"\n"+output6;
+        System.out.println(output);
+        return output;
     } 
 }
